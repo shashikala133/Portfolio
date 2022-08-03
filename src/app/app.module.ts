aee106dddfae95f2aef709cfcc5dfc203da76fdb
shashikala133 <sshashikala1330@gmail.com>
2022-08-03 14:18:31 +0530

    init

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,26 @@
+
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { NavComponent } from './nav/nav.component';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FooterComponent } from './footer/footer.component';
+import { SkillComponent } from './skill/skill.component';
+import { ProjectComponent } from './project/project.component';
+import { AboutMeComponent } from './about-me/about-me.component';
+import { CertificationComponent } from './certification/certification.component';
+import { EducationDetailsComponent } from './education-details/education-details.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponent,
+    FooterComponent,
+    SkillComponent,
+    ProjectComponent,
+    AboutMeComponent,
+    CertificationComponent,
+    EducationDetailsComponent
   ],
   imports: [
     BrowserModule,
